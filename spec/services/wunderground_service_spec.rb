a9f001342df3b879e21d23f309fe568525651ff3
Brad <bgreen3@uoregon.edu>
2017-01-02 22:29:32 -0700

    Passing test for Wunderground service

--- spec/services/wunderground_service_spec.rb
+++ spec/services/wunderground_service_spec.rb
@@ -5,14 +5,15 @@ describe "WundergroundService" do
     it "returns 10-day forecast for location" do
       VCR.use_cassette("#forecast_10day") do
         forecast_10day = WundergroundService.new.forecast_10day("80210")
+        forecast_day1 = WundergroundService.new.forecast_10day("80210")[:forecast][:simpleforecast][:forecastday].first
 
-        expect(forecast_10day).to be_an(Array)
-        expect(forecast_10day).to have_key(:weekday)
-        expect(forecast_10day).to have_key(:monthname)
-        expect(forecast_10day).to have_key(:day)
-        expect(forecast_10day).to have_key(:high)
-        expect(forecast_10day).to have_key(:low)
-        expect(forecast_10day).to have_key(:conditions)
+        expect(forecast_10day).to be_a(Hash)
+        expect(forecast_day1[:date]).to have_key(:weekday)
+        expect(forecast_day1[:date]).to have_key(:monthname)
+        expect(forecast_day1[:date]).to have_key(:day)
+        expect(forecast_day1[:high]).to have_key(:fahrenheit)
+        expect(forecast_day1[:low]).to have_key(:fahrenheit)
+        expect(forecast_day1).to have_key(:conditions)
 
       end
     end
